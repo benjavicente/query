3b2d1826d08b8377fef928a69b63c638715f12d4
Arnoud de Vries <6420061+arnoud-dv@users.noreply.github.com>
2025-11-23 02:17:10 +0100

    Fix isRestoring() handling

--- packages/angular-query-experimental/src/__tests__/inject-query.test.ts
+++ packages/angular-query-experimental/src/__tests__/inject-query.test.ts
@@ -560,11 +560,19 @@ describe('injectQuery', () => {
 
     test('should throw when throwOnError is true', async () => {
       const zone = TestBed.inject(NgZone)
-      const errorPromise = new Promise<Error>((resolve) => {
+      const zoneErrorPromise = new Promise<Error>((resolve) => {
         const sub = zone.onError.subscribe((error) => {
           sub.unsubscribe()
           resolve(error as Error)
         })
+      })
+      let handler: ((error: Error) => void) | null = null
+      const processErrorPromise = new Promise<Error>((resolve) => {
+        handler = (error: Error) => {
+          process.off('uncaughtException', handler!)
+          resolve(error)
+        }
+        process.on('uncaughtException', handler)
       })
 
       @Component({
@@ -584,17 +592,32 @@ describe('injectQuery', () => {
 
       TestBed.createComponent(TestComponent).detectChanges()
 
-      await vi.runAllTimersAsync()
-      await expect(errorPromise).resolves.toEqual(Error('Some error'))
+      try {
+        await vi.runAllTimersAsync()
+        await expect(zoneErrorPromise).resolves.toEqual(Error('Some error'))
+        await expect(processErrorPromise).resolves.toEqual(Error('Some error'))
+      } finally {
+        if (handler) {
+          process.off('uncaughtException', handler)
+        }
+      }
     })
 
     test('should throw when throwOnError function returns true', async () => {
       const zone = TestBed.inject(NgZone)
-      const errorPromise = new Promise<Error>((resolve) => {
+      const zoneErrorPromise = new Promise<Error>((resolve) => {
         const sub = zone.onError.subscribe((error) => {
           sub.unsubscribe()
           resolve(error as Error)
         })
+      })
+      let handler: ((error: Error) => void) | null = null
+      const processErrorPromise = new Promise<Error>((resolve) => {
+        handler = (error: Error) => {
+          process.off('uncaughtException', handler!)
+          resolve(error)
+        }
+        process.on('uncaughtException', handler)
       })
 
       @Component({
@@ -614,8 +637,15 @@ describe('injectQuery', () => {
 
       TestBed.createComponent(TestComponent).detectChanges()
 
-      await vi.runAllTimersAsync()
-      await expect(errorPromise).resolves.toEqual(Error('Some error'))
+      try {
+        await vi.runAllTimersAsync()
+        await expect(zoneErrorPromise).resolves.toEqual(Error('Some error'))
+        await expect(processErrorPromise).resolves.toEqual(Error('Some error'))
+      } finally {
+        if (handler) {
+          process.off('uncaughtException', handler)
+        }
+      }
     })
   })
 
