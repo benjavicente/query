3791f1981035bd13f5a297e1b5687dac1e29047e
Benjamín Vicente <projects@benjavicente.dev>
2025-12-14 01:40:39 -0300

    remove status discriminator tests

--- packages/angular-query-experimental/src/__tests__/inject-infinite-query.test-d.ts
+++ packages/angular-query-experimental/src/__tests__/inject-infinite-query.test-d.ts
@@ -21,23 +21,4 @@ describe('injectInfiniteQuery', () => {
       expectTypeOf(query.error()).toEqualTypeOf<null>()
     }
   })
-
-  test('should narrow type with status', () => {
-    const query = injectInfiniteQuery(() => ({
-      queryKey: ['infiniteQuery'],
-      queryFn: () => Promise.resolve('data'),
-      initialPageParam: 1,
-      getNextPageParam: () => 12,
-    }))
-
-    if (query.status() === 'success') {
-      expectTypeOf(query.data()).toEqualTypeOf<InfiniteData<string, unknown>>()
-      expectTypeOf(query.error()).toEqualTypeOf<null>()
-    } else if (query.status() === 'error') {
-      expectTypeOf(query.error()).toEqualTypeOf<Error>()
-    } else if (query.status() === 'pending') {
-      expectTypeOf(query.data()).toEqualTypeOf<undefined>()
-      expectTypeOf(query.error()).toEqualTypeOf<null>()
-    }
-  })
 })
